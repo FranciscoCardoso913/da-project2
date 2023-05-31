6f878e39f0f9919a3fe49cb1fc76938473d11b70
DiogoAlexandreOliveiraDaSilva <afkflex@gmail.com>
2023-05-31 14:48:49 +0100

    Kruskal MST

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,11 @@
 #include "services/Program.h"
 using namespace std;
 
-    int main()
-    {
-        system("clear");
-        Program program;
-        program.run();
-        system("clear");
-        return 0;
-    }
-
-
+int main()
+{
+    system("clear");
+    Program program;
+    program.run();
+    system("clear");
+    return 0;
+}
