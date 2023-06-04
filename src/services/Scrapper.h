1f5d53a4bc342e4ae760a66aadd9efb33a0bec9a
ze <joseapmartins2003@gmail.com>
2023-06-04 14:00:18 +0100

    Changed scrapper stuff

--- src/services/Scrapper.h
+++ src/services/Scrapper.h
@@ -18,9 +18,10 @@ public:
      * @param graph graph were all the information is going to be stored
      * @param station_file the file containing the stations
      * @param edge_file the file containing the edges
+     * @param ignore if the user wants to ignore the first line of the file
      * @brief Complexity O(V+E) being V the number of stations and E the number of edges that exists
      */
-    void scrape(Graph &graph, string station_file, string edge_file,int option);
+    void scrape(Graph &graph, string station_file, string edge_file,int option, bool ignore);
 
     /**
      * @brief Scrapes the information from the stations and stores it in the graph
@@ -46,7 +47,7 @@ public:
      */
     void getValue(string &value, istringstream &data);
 
-    void scrapeEdgesWithoutNodes(Graph &graph,string &edges_value );
+    void scrapeEdgesWithoutNodes(Graph &graph,string &edges_value, bool ignore);
 };
 
 #endif // DA_PROJECT1_SCRAPPER_H
